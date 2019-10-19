4ef85c7a4f941e66d2a144b52b862f2b78f56e2f
Lost-Figo <562126277@qq.com>
2019-10-19 10:10:41 +0800

    建站

---
+++ src/entity/TmUser.java
@@ -0,0 +1,31 @@
+package entity;
+
+public class TmUser {
+    private int uid;
+    private String name;
+    private String password;
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
